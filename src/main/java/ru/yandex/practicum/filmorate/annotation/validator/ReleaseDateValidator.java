ce65b23d785091f33432b044d1ab71a1ad8b32c2
Pe4EHKA <Pe4EHKA.COM@yandex.ru>
2024-07-20 18:20:09 +0500

    CheckStyle fix

--- src/main/java/ru/yandex/practicum/filmorate/annotation/validator/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/validator/ReleaseDateValidator.java
@@ -7,7 +7,7 @@ import ru.yandex.practicum.filmorate.annotation.ReleaseDateValidation;
 import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDateValidation, LocalDate> {
-    static final LocalDate BIRTHDAY_OF_CINEMA= LocalDate.of(1895, 12, 28);
+    static final LocalDate BIRTHDAY_OF_CINEMA = LocalDate.of(1895, 12, 28);
 
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
